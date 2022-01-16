c30582650e505f37eb4ada714882f82414eedf29
BrendanSkyLiang <brendanliang@gmail.com>
2022-01-16 15:20:08 -0800

    weather

---
+++ geoapi.js
@@ -0,0 +1,46 @@
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Accesing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const apiKey = '1142346d1944719788744d7d9581c093';
+      const base = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + long + "&appid=" + apiKey + "&units=metric";
+
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          const tempC = data.main.temp;
+          const place = data.name;
+          const icon = data.weather[0].icon;
+          const sunrise = data.sys.sunrise;
+          const sunset = data.sys.sunset;
+
+          const iconURL = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
+
+          // Converting Epoch(Unix) time to GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+          
+          // Interacting with DOM to show data
+          tempC.textContent = `${tempC.toFixed(2)} °C`;
+          document.getElementById("temperature").innerHTML = tempC.toFixed(1) + '°C'
+          var img = document.createElement('img')
+          img.src = iconURL;
+          document.getElementById("tempIcon").appendChild(img);
+          // loc.textContent = `${place}`;
+          // desc.textContent = `${description}`;
+
+          // sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+          // sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+        });
+    });
+  }
+});
